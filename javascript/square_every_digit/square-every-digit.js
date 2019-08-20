ebc234cfd750b5f3802f577d2e9e140a9302abdb
Krillian <krillian@gmx.de>
2019-08-20 18:21:08 +0200

    basic setup for kata

---
+++ javascript/square_every_digit/square-every-digit.js
@@ -0,0 +1,16 @@
+// function only declared because codewars requires it
+function squareDigits(num){
+    return squareEveryDigit.squareDigits(num);
+}
+
+const squareEveryDigit = {
+
+    squareDigits: function(integer){
+        // dummy implementation
+    }
+}
+
+
+if(exports){
+    module.exports = squareEveryDigit;
+}
